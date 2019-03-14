f1120f6d98df5409bee4ad9763962c4e9e285291
David Freese <w1hkj@bellsouth.net>
2019-03-14 06:52:00 -0600

    tod clock start
      * move to after debug init

--- src/main.cxx
+++ src/main.cxx
@@ -773,7 +773,7 @@ int main(int argc, char ** argv)
 	program_start_time = time(0);
 
 	// ztimer must be run by FLTK's timeout handler
-	TOD_init(); // initialize time of day thread
+//	TOD_init(); // initialize time of day thread
 //	ztimer((void*)true);
 
 	active_modem = new NULLMODEM;
@@ -1032,6 +1032,7 @@ int main(int argc, char ** argv)
 		cerr << error << '\n';
 		debug::stop();
 	}
+	TOD_init(); // initialize time of day thread
 
 	LOG_INFO("appname: %s", appname.c_str());
 
